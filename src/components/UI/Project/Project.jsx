5409b57a85d352a786767526fcf0444901136f0b
King-Mike <nsubugamike021@gmail.com>
2020-04-12 11:47:33 +0300

    Changed card style

--- src/components/UI/Project/Project.jsx
+++ src/components/UI/Project/Project.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 const Project = (props) => {
     const { title, description, stack, backgroundImageURL} = props.project;
+    const width = 15;
     return ( 
         <div>
-            
-        <div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-        <div className="project" style={{backgroundImage: backgroundImageURL}}>
-            <div className="desc">
-                <div className="con">
-                    <h3><a href="work.html">{title}</a></h3>
-                    <span>{stack}</span>
-                    <p>{description}</p>
-                    <p className="icon">
-                        <span><a href="#"><i className="icon-eye" /> 100</a></span>
-                    </p>
-                </div>
-            </div>
+            <div className="col-md-4">
+            <div class="card" style={{width: width + 'em'}}>
+            <img class="card-img-top" src="https://source.unsplash.com/random/286x180" alt="Card image cap"/>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+         </div>
         </div>
-    </div>
+            </div>
     </div>
      );
 }
